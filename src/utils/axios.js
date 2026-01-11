c95d5f86cab7c2f0db0c9883115bb7bc2e8c70d4
Sakhra-butt <sakhranoor@gmail.com>
2026-01-11 23:25:31 +0300

    Fix Netlify SPA routing

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,8 +2,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:  `${process.env.REACT_APP_API_URL}/api`,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
+
 
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
